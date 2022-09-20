84a0f4e621e2262aef18bf1df144911b92cf3a2e
mohammed <moshoodmohammed6026@gmail.com>
2022-09-20 00:18:43 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - function should print only one character out of two
+ * puts2 - a function should print only one character out of two
  * starting with the first one
  * @str: input
  * Return: print
@@ -13,9 +13,9 @@ void puts2(char *str)
 	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
-			putchar(str[i]);
+			_putchar(str[i]);
 		else
 			continue;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
