2970732ce3ba916237ee3cea069ef9f8f40cb296
mohammed <moshoodmohammed6026@gmail.com>
2022-09-21 09:13:45 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,11 @@
 
 /**
  * main - alphabet
- * print_alphabet_x10 - print a - z ten times
+ * print_alphabet_*10 - print alphabet
+ *
+ * Return: Always 0
  */
-void print_alphabet_x10(void)
+void print_alphabet_*10(void)
 {
 	int i;
 	char j;
