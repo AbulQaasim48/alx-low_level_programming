1dbb920daf328e31a63f322c807f223c3f0abcf5
mohammed <moshoodmohammed6026@gmail.com>
2022-09-22 10:28:02 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,19 +8,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int *start_c, *end_c, c;
-	int i;
+	int tmp, index;
 
-	start_c = a;
-	end_c = a;
-
-	for (i = 0; i < n - 1; i++)
+	for (index = n - 1; index >= n / 2; index--)
 	{
-		c = *end_c;
-		*end_c = *start_c;
-		*start_c = c;
-
-		start_c++;
-		end_c--;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
 	}
 }
