cc71f7aa8be464c8af203f94dd241af9e75f1b35
mohammed <moshoodmohammed6026@gmail.com>
2022-09-22 00:24:32 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,8 +14,8 @@ void print_triangle(int size)
 	if (size > 0)
 		for (i = size; i > 0; i--)
 		{
-			for (j = 1; j<= size; j++)
-				if (j >= i)
+			for (j = 1; j <= size; j++)
+				if (j > i)
 		_putchar('#');
 				else
 		_putchar(' ');
